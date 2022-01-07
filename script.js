6b5b0f2818dca39c67fe28937b417e1b3362c6a5
sivaneko <96870535+sivaneko@users.noreply.github.com>
2022-01-07 18:26:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.display').append("<p class='block'>129898</p>");
+});
+$('#button').click(function(){
+    function getRandam(n, m, x){
+        for (let i = 0 ; i < x ; i++){
+          let num = Math.floor(Math.random() * (m + 1 - n))+n;
+          $('.display').append("<p class='block'>"+num+"</p>");
+        };
+    };
+    let min=Number($('#min').val());
+    let max=Number($('#max').val());
+    let count=Number($('#count').val());
+    getRandam(min,max,count);
+});
+function reset(){
+    $('.block').remove();
+};
